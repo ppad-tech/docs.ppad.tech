365cea39bc1a0147dff3bf8b0d9ac111c615f81f
Jared Tobin <jared@jtobin.io>
2024-10-04 18:18:19 +0400

    flake: init

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "docs.ppad.tech";
+
+  inputs = {
+    ppad-sha256 = {
+      type = "git";
+      url  = "git://git.ppad.tech/sha256.git";
+      ref  = "master";
+    };
+    ppad-hmac-drbg = {
+      type = "git";
+      url  = "git://git.ppad.tech/hmac-drbg.git";
+      ref  = "master";
+    };
+    ppad-csecp256k1 = {
+      type = "git";
+      url  = "git://git.ppad.tech/csecp256k1.git";
+      ref  = "master";
+    };
+    flake-utils.follows = "ppad-sha256/flake-utils";
+    nixpkgs.follows = "ppad-sha256/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils
+            , ppad-sha256, ppad-hmac-drbg, ppad-csecp256k1 }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hlib = pkgs.haskell.lib;
+
+        sha256 = ppad-sha256.packages.${system}.default;
+        hmac-drbg = ppad-hmac-drbg.packages.${system}.default;
+        csecp256k1 = ppad-csecp256k1.packages.${system}.default;
+
+        hpkgs = pkgs.haskell.packages.ghc981.extend (new: old: {
+          ppad-sha256 = sha256;
+          ppad-hmac-drbg = hmac-drbg;
+          ppad-csecp256k1 = csecp256k1;
+        });
+
+        cc    = pkgs.stdenv.cc;
+        ghc   = hpkgs.ghc;
+        cabal = hpkgs.cabal-install;
+      in
+        {
+          packages.default = hpkgs.ppad-sha256; # arbitrary
+
+          devShells.default = hpkgs.shellFor {
+            packages = p: [
+              p.ppad-sha256
+              p.ppad-hmac-drbg
+              p.ppad-csecp256k1
+            ];
+
+            buildInputs = [
+              cabal
+              cc
+            ];
+
+            inputsFrom = builtins.attrValues self.packages.${system};
+
+            shellHook = ''
+              PS1="[docs.ppad.tech] \w$ "
+              echo "entering ${system} shell, using"
+              echo "cc:    $(${cc}/bin/cc --version)"
+              echo "ghc:   $(${ghc}/bin/ghc --version)"
+              echo "cabal: $(${cabal}/bin/cabal --version)"
+            '';
+          };
+        }
+      );
+}
+
