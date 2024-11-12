04e811e73a73e6c0f5975cdeeee340b1027f1b39
Jared Tobin <jared@jtobin.io>
2024-11-12 12:53:22 +0400

    flake: lockfile

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
       url  = "git://git.ppad.tech/sha512.git";
       ref  = "master";
     };
+    ppad-ripemd160 = {
+      type = "git";
+      url  = "git://git.ppad.tech/ripemd160.git";
+      ref  = "master";
+    };
     ppad-hmac-drbg = {
       type = "git";
       url  = "git://git.ppad.tech/hmac-drbg.git";
@@ -37,7 +42,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils
-            , ppad-sha256, ppad-sha512
+            , ppad-sha256, ppad-sha512, ppad-ripemd160
             , ppad-hmac-drbg, ppad-csecp256k1, ppad-secp256k1 }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -46,6 +51,7 @@
 
         sha256 = ppad-sha256.packages.${system}.default;
         sha512 = ppad-sha512.packages.${system}.default;
+        ripemd160 = ppad-ripemd160.packages.${system}.default;
         hmac-drbg = ppad-hmac-drbg.packages.${system}.default;
         csecp256k1 = ppad-csecp256k1.packages.${system}.default;
         secp256k1 = ppad-secp256k1.packages.${system}.default;
@@ -53,6 +59,7 @@
         hpkgs = pkgs.haskell.packages.ghc981.extend (new: old: {
           ppad-sha256 = sha256;
           ppad-sha512 = sha512;
+          ppad-ripemd160 = ripemd160;
           ppad-hmac-drbg = hmac-drbg;
           ppad-csecp256k1 = csecp256k1;
           ppad-secp256k1 = secp256k1;
@@ -67,6 +74,7 @@
 
           packages.ppad-sha256 = hpkgs.ppad-sha256;
           packages.ppad-sha512 = hpkgs.ppad-sha512;
+          packages.ppad-ripemd160 = hpkgs.ppad-ripemd160;
           packages.ppad-hmac-drbg = hpkgs.ppad-hmac-drbg;
           packages.ppad-csecp256k1 = hpkgs.ppad-csecp256k1;
           packages.ppad-secp256k1 = hpkgs.ppad-secp256k1;
@@ -75,6 +83,7 @@
             packages = p: [
               p.ppad-sha256
               p.ppad-sha512
+              p.ppad-ripemd160
               p.ppad-hmac-drbg
               p.ppad-csecp256k1
               p.ppad-secp256k1
